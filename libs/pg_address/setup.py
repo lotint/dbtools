6a0e3672e757c556ac3c82d04b0adb4cf8b88e50
Vitaly Pikulik <v.pikulik@gmail.com>
2017-01-16 13:53:09 +0100

    Create setup.py

---
+++ libs/pg_address/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(
+    name='pg_address',
+    version='0.0.1',
+    description='SqlAlchemy type for pg_address postgres type',
+    packages=['pg_address'],
+    install_requires=[
+        'psycopg2==2.6.2',
+        'SQLAlchemy==1.1.4',
+    ],
+)
