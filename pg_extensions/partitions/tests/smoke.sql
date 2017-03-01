2f2f480d925241766a2c9d58b57d58e670fea787
Vitaly Pikulik <v.pikulik@gmail.com>
2017-03-01 17:18:19 +0100

    Chnage test to use weekly partitions

--- pg_extensions/partitions/tests/smoke.sql
+++ pg_extensions/partitions/tests/smoke.sql
@@ -14,15 +14,15 @@ CREATE EXTENSION partitions;
 
 CREATE OR REPLACE FUNCTION set_up()
 RETURNS void AS $$
-    DROP TABLE IF EXISTS items CASCADE;
-    CREATE TABLE items (
+    DROP TABLE IF EXISTS items_weekly CASCADE;
+    CREATE TABLE items_weekly (
         id SERIAL,
         title varchar(255),
         created timestamp,
         PRIMARY KEY (id),
         CONSTRAINT title_unique UNIQUE (title)
     );
-    SELECT init_partitions('items', 'DDD_YY');
+    SELECT init_partitions('items_weekly', 'YYYY_WW');
 $$ LANGUAGE SQL;
 
 
@@ -32,33 +32,33 @@ SELECT plan(4 + 4 + 1);
 
 -- check schema and simple insert
 SELECT set_up();
-INSERT INTO items (title, created) VALUES ('row1', '2016-01-10'::timestamp); 
+INSERT INTO items_weekly (title, created) VALUES ('row1', '2016-01-10'::timestamp);
 -- schema
 SELECT tables_are(
     'public',
-    ARRAY['items', 'items_p_010_16']
+    ARRAY['items_weekly', 'items_weekly_p_2016_02']
 );
-SELECT has_pk('items_p_010_16', 'partition should copy PK from super');
-SELECT col_is_unique('items_p_010_16', 'title', 'partition should copy unique from super');
+SELECT has_pk('items_weekly_p_2016_02', 'partition should copy PK from super');
+SELECT col_is_unique('items_weekly_p_2016_02', 'title', 'partition should copy unique from super');
 -- data
 SELECT is(
-    (SELECT count(*) FROM items_p_010_16)::int,
+    (SELECT count(*) FROM items_weekly_p_2016_02)::int,
     1
 );
 
 -- check insert of multiple values
 SELECT set_up();
-INSERT INTO items (title, created) VALUES ('row1', '2016-01-10'::timestamp), ('row2', '2016-01-20'::timestamp); 
-SELECT is((SELECT count(*) FROM items_p_010_16)::int, 1);
-SELECT is((SELECT count(*) FROM items_p_020_16)::int, 1);
-SELECT is((SELECT id FROM items_p_010_16 WHERE title='row1'), 1);
-SELECT is((SELECT id FROM items_p_020_16 WHERE title='row2'), 2);
+INSERT INTO items_weekly (title, created) VALUES ('row1', '2016-01-10'::timestamp), ('row2', '2016-01-20'::timestamp);
+SELECT is((SELECT count(*) FROM items_weekly_p_2016_02)::int, 1);
+SELECT is((SELECT count(*) FROM items_weekly_p_2016_03)::int, 1);
+SELECT is((SELECT id FROM items_weekly_p_2016_02 WHERE title='row1'), 1);
+SELECT is((SELECT id FROM items_weekly_p_2016_03 WHERE title='row2'), 2);
 
 -- get master table constraint
 SELECT set_up();
-INSERT INTO items (title, created) VALUES ('row1', '2016-01-16'::timestamp);
+INSERT INTO items_weekly (title, created) VALUES ('row1', '2016-01-16'::timestamp);
 SELECT is(
-    (SELECT get_master_constraint('items_p_016_16_title_key', 'items')),
+    (SELECT get_master_constraint('items_weekly_p_2016_03_title_key', 'items_weekly')),
     'title_unique'
 );
 
