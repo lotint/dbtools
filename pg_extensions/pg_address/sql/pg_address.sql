2f18087227018f041a4fe47c9088292c2a79b213
Vitaly Pikulik <v.pikulik@gmail.com>
2017-08-29 11:42:27 +0200

    Add new columns to the end of the type

--- pg_extensions/pg_address/sql/pg_address.sql
+++ pg_extensions/pg_address/sql/pg_address.sql
@@ -6,9 +6,10 @@ CREATE TYPE pg_address AS (
     region VARCHAR(255),
     city VARCHAR(255),
     zip_code VARCHAR(255),
-    suburb VARCHAR(255),
     street TEXT,
     num VARCHAR(255),
+    -- added in 0.0.3
+    suburb VARCHAR(255),
     lat DOUBLE PRECISION,
     lon DOUBLE PRECISION
 );
