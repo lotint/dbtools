40614319c511476db91f3ef368b28ae0bdca18c0
Vitaly Pikulik <v.pikulik@gmail.com>
2017-08-29 12:13:50 +0200

    Migrate script

---
+++ pg_extensions/pg_address/sql/pg_address--0.0.2.sql
@@ -0,0 +1,11 @@
+
+\echo Use "CREATE EXTENSION pg_address" to load this file. \quit
+
+CREATE TYPE pg_address AS (
+    country VARCHAR(255),
+    region VARCHAR(255),
+    city VARCHAR(255),
+    zip_code VARCHAR(255),
+    street TEXT,
+    num VARCHAR(255)
+);
