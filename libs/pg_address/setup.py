cfe3cdaffa5776be9e2e1e246cb1b66906f2a8c0
Vitaly Pikulik <v.pikulik@gmail.com>
2019-09-23 14:10:55 +0200

    Update pg_address python requirements

--- libs/pg_address/setup.py
+++ libs/pg_address/setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
     name='pg_address',
-    version='0.0.3',
+    version='0.0.4',
     description='SqlAlchemy type for pg_address postgres type',
     packages=['pg_address'],
     install_requires=[
-        'psycopg2>=2.6.2',
+        'psycopg2-binary>=2.7.5',
         'SQLAlchemy>=1.1.4',
     ],
 )
